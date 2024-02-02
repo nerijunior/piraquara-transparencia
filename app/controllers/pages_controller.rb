e78fc0c1bfbc686ebbf9b6f21765878aec7c8e04
Neri J. Jakubowski Jr <neri@nerijunior.com>
2024-02-02 04:50:17 -0300

    visualizao em tabela

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ require 'faraday'
 
 class PagesController < ApplicationController
   def home
-    @servidores = Servidor.order(:salario_liquido, :desc).all
+    @servidores = Servidor.order(:salario_liquido).all
   end
 end
